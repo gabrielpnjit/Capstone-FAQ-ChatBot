3a2aa73144a62454a1a804505e858e7d60d164f1
Gabriel Pascual <gzp2@njit.edu>
2024-04-20 00:08:46 -0400

    updated log schema on dashboard site

--- dashboard-site/server/db/connection.js
+++ dashboard-site/server/db/connection.js
@@ -1,24 +1,26 @@
 import mongoose from 'mongoose';
+const URI = process.env.ATLAS_URI;
+
+mongoose.connect(URI, {})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+const logSchema = new mongoose.Schema({
+  questionId: String,
+  question: String,
+  answer: String,
+  feedback: String,
+  timestamp: String,
+  sources: [String]
+});
 
 const fileSchema = new mongoose.Schema({
   SharedID: mongoose.Schema.Types.ObjectId, // Change to ObjectId type
   filename: String,
   content: String,
 })
-const logSchema = new mongoose.Schema({
-	question: String,
-	Answer: String,
-  feedback: String,
-  timestamp: String,
-  sources:[String]
-})
 
 const Logs = mongoose.model('logs', logSchema);
 const File = mongoose.model('File', fileSchema);
 
-const URI = process.env.ATLAS_URI;
-mongoose.connect(URI, {})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
-
-  export { File, Logs};
+export { File, Logs};
