c63e1c1545c1db2c9df4b39e3f0f5ba810ace3e9
Gabriel Pascual <gzp2@njit.edu>
2024-04-27 00:48:22 -0400

    adjusted details and main box

--- dashboard-site/client/src/App.jsx
+++ dashboard-site/client/src/App.jsx
@@ -11,14 +11,13 @@ const App = () => {
         <div className="p-6 bg-gray-300 rounded-lg">
           <div className="text-left">
             <h5 className="font-sans-serif text-5xl font-semibold">
-              Leverage the power of LLMs to answer your Capstone questions!
+              Leverage the power of RAG and LLMs to answer your Capstone questions!
             </h5>
             <h4 className="pt-6 text-lg">
               To start asking the bot questions use`/ask` and ask a question to get immediate answers!
               The bot will respond with an answer and provide the sources that it found the answer from.<br></br><br/>
               Provide the bot feedback by selecting the `Good` or `Bad` buttons provided by the message.<br/>
               View the chat logs, as well as other details under the "View Logs" Tab of this website.<br/><br/>
-              Reduce asking questions you've already asked to avoid taking up time.<br></br><br></br>
               If you're interested in learning more about the project, visit the GitHub <a  style={{color: "#3B81F6"}}href="https://github.com/gabrielpnjit/Capstone-FAQ-ChatBot">here.</a><br/><br/>
               
             </h4>
@@ -41,19 +40,22 @@ const App = () => {
       <div className="p-6 bg-gray-300 rounded-lg">
         <h3 className="font-semibold text-3xl">Goals</h3>
         <h2 className="text-xl">
-          -Reduce Moderator Workloads in discord<br></br>
+          -Reduce moderator workloads in Discord<br></br>
           -Provide answers for students<br></br>
-          -Streamline the learning process<br></br>
           -Adaptable knowledge base<br></br>
+          -View bot logs<br></br>
         </h2>
       </div>
       <div className="p-6 bg-gray-300 rounded-lg">
         <h3 className="font-semibold text-3xl">Details</h3>
         <h2 className="text-xl"> 
           -MERN Stack Website<br></br>
-          -Pinecone Vectore Database<br></br>
-          -Langserve Embedding API<br></br>
-          -Node JS Discord Bot<br></br>
+          -OpenAI Embeddings<br></br>
+          -GPT-3.5 Turbo LLM<br></br>
+          -Cohere Reranker<br></br>
+          -Pinecone Vector Database<br></br>
+          -LangChain Framework<br></br>
+          -Discord.js Discord Bot<br></br>
         </h2>
       </div>
     </div>
