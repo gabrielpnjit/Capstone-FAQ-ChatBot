f30585510aa8dbebbe4813cea6390aed00fa926f
Gabriel Pascual <gzp2@njit.edu>
2024-04-30 21:53:47 -0400

    removed auth0 lines

--- dashboard-site/server/server.js
+++ dashboard-site/server/server.js
@@ -2,23 +2,23 @@ import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
 import documents from "./routes/document.js";
-import { auth } from 'express-openid-connect';
+// import { auth } from 'express-openid-connect';
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.SESSION_SECRET,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
-};
+// const config = {
+//   authRequired: false,
+//   auth0Logout: true,
+//   secret: process.env.SESSION_SECRET,
+//   baseURL: process.env.BASE_URL,
+//   clientID: process.env.AUTH0_CLIENT_ID,
+//   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+// };
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(auth(config));
+// app.use(auth(config));
 // app.use("/record", records);
 app.use("/document", documents);
 
